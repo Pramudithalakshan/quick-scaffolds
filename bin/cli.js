f6008367f7bd1d33cd843eb229a7aee16fbb0533
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-04-01 17:31:53 +0530

    feat: add support for scaffolding React projects and installing dependencies

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import { execSync } from 'node:child_process';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -23,7 +23,7 @@ async function main() {
             type: "list",
             choices: [
                 { name: 'Static HTML/CSS/JS', value: 'static' },
-                { name: 'React starter', value: 'react'}
+                { name: 'React starter', value: 'react' }
             ],
         });
         let templateFolder = '';
@@ -31,6 +31,11 @@ async function main() {
             templateFolder = 'html-template';
         }
 
+        if (projectType === 'react') {
+            templateFolder = 'react-template';
+            await reactApp(name, templateFolder);
+            return;
+        }
 
         const projectName = name;
         const targetPath = path.join(process.cwd(), projectName);
@@ -38,13 +43,52 @@ async function main() {
         const templatePath = path.join(__dirname, '../templates', templateFolder);
         await fs.mkdir(targetPath, { recursive: true });
         await fs.cp(templatePath, targetPath, { recursive: true });
-        console.log(`Successfully scaffolded the project ${projectName}`)
+        console.log(`Successfully scaffolded the HTMl project - ${projectName}`)
 
     } catch (err) {
         console.error(`Error - ${err.message}`)
     }
 }
 
+async function reactApp(projectName, templateFolder) {
+    try {
+        const targetPath = path.join(process.cwd(), projectName);
+        const templatePath = path.join(__dirname, '../templates', templateFolder);
+        await fs.mkdir(targetPath, { recursive: true });
+        await fs.cp(templatePath, targetPath, { recursive: true })
 
+        const filesToUpdate = [
+            path.join(targetPath, 'package.json'),
+            path.join(targetPath, '/public/index.html')
+        ];
+        for (const filePath of filesToUpdate) {
+            try {
+                const content = await fs.readFile(filePath, 'utf-8');
+                const updateContent = content.replaceAll('{{PROJECT_NAME}}', projectName);
+                await fs.writeFile(filePath, updateContent);
+            } catch (err) {
+                console.error(err);
+                console.error(`File path ${path.basename(filePath)} not found`);
+            }
+        }
+
+        console.log(`Successfully scaffolded the React starter project - ${projectName}`)
+        console.log('📦 Installing dependencies... This may take a minute.');
+        try {
+            execSync('npm install', {
+                cwd: targetPath,
+                stdio: 'inherit'
+            });
+            console.log('✅ Dependencies installed successfully!');
+        } catch (err){
+            console.error(err);
+            console.log('⚠️ Could not install dependencies automatically.');
+            console.log('Please run "npm install" manually inside your project folder.');
+        }
+
+    } catch (err) {
+        console.error(`Error - ${err.message}`);
+    }
+}
 
 main();
